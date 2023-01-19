2c27d8aab4d5d34c069ba7cf4b6cb1f56898d4d4
myyrakle <sssang97@naver.com>
2023-01-20 02:38:54 +0900

    add: github login button

---
+++ src/routes/redirect/github-login/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+	<title>About</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Github Login Redirect Page</h1>
+
+	<a href="https://github.com/login/oauth/authorize?client_id=09c53661153af8c5b13c">Github Login</a>
+</div>
