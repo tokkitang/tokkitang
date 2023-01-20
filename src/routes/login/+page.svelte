849851201d68733d29539919b9a41e85d443570b
myyrakle <sssang97@naver.com>
2023-01-21 02:12:15 +0900

    change: github login redirect uri

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -7,7 +7,7 @@
 	<h1>Login Page</h1>
 
 	<a
-		href="https://github.com/login/oauth/authorize?client_id=09c53661153af8c5b13c&redirect_uri=https://ksauqt5f5er2djql3atquzas4e0ofpla.lambda-url.ap-northeast-2.on.aws/redirect/github/code"
+		href="https://github.com/login/oauth/authorize?client_id=09c53661153af8c5b13c&redirect_uri=https://ksauqt5f5er2djql3atquzas4e0ofpla.lambda-url.ap-northeast-2.on.aws/redirect/github"
 		>Github Login</a
 	>
 </div>
