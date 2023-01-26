8df7c7e5ff610a85398195d092a7ffb8e4b6c577
myyrakle <sssang97@naver.com>
2023-01-27 01:22:27 +0900

    feat: github login & signup

--- src/routes/redirect/github/+page.ts
+++ src/routes/redirect/github/+page.ts
@@ -3,6 +3,7 @@ import { redirect, type LoadEvent } from '@sveltejs/kit';
 import { getGithubAccessToken } from '../../../lib/api/auth/get-github-token';
 import { getGithubUser } from '../../../lib/api/auth/get-github-user';
 import { githubLogin } from '../../../lib/api/auth/github-login';
+import { githubSignup } from '../../../lib/api/auth/github-signup';
 import { setCookie } from '../../../lib/utils/cookie';
 
 export const prerender = true;
@@ -14,6 +15,12 @@ export async function load({ url }: LoadEvent) {
 	if (!code) {
 		throw redirect(302, '/');
 	}
+
+	// 1. github code로 access token 획득
+	// 2. access token으로 정보 조회
+	// 3.
+	//    3.1. 정보가 온전한 경우 가입 API 바로 전송
+	//    3.2. 정보가 온전하지 않은 경우 기존의 가입페이지를 띄움
 
 	try {
 		const githubTokenResponse = await getGithubAccessToken(code);
@@ -27,13 +34,37 @@ export async function load({ url }: LoadEvent) {
 			if (githubLoginResponse.need_signup) {
 				const githubUserInfo = await getGithubUser(githubAccessToken);
 				if (
-					githubUserInfo.email == null ||
-					githubUserInfo.name == null ||
-					githubUserInfo.id == null
+					githubUserInfo.email != null &&
+					githubUserInfo.name != null &&
+					githubUserInfo.id != null
 				) {
+					const githubSignupResponse = await githubSignup(
+						githubUserInfo.name,
+						githubUserInfo.email,
+						githubAccessToken
+					);
+
+					if (githubSignupResponse.success) {
+						setCookie('access_token', githubSignupResponse.access_token);
+					} else if (githubSignupResponse.email_duplicate) {
+						alert('이미 가입된 이메일입니다.');
+						throw redirect(302, '/');
+					} else {
+						alert('가입 실패');
+						throw redirect(302, '/');
+					}
 				}
 
-				throw redirect(302, '/signup?github=true');
+				let query = `?github=true`;
+
+				if (githubUserInfo.email != null) {
+					query += `&email=${githubUserInfo.email}`;
+				}
+				if (githubUserInfo.name != null) {
+					query += `&nickname=${githubUserInfo.name}`;
+				}
+
+				throw redirect(302, `/signup${query}`);
 			} else {
 				alert('로그인 실패. 존재하지 않는 계정');
 				throw redirect(302, '/');
@@ -42,10 +73,4 @@ export async function load({ url }: LoadEvent) {
 	} catch (error) {
 		console.error(error);
 	}
-
-	// TODO: code로 access token 획득
-	// TODO: access token으로 정보 조회
-	// TODO:
-	//    1. 정보가 온전한 경우 가입 API 바로 전송
-	//    2. 정보가 온전하지 않은 경우 기존의 가입페이지를 띄움
 }
