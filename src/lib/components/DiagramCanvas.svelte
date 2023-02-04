878b4c90c725b48628a80849079aa177d200a400
myyrakle <sssang97@naver.com>
2023-02-05 00:02:47 +0900

    impl: input text

--- src/lib/components/DiagramCanvas.svelte
+++ src/lib/components/DiagramCanvas.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import Konva from 'konva';
+	import { makeInputText } from '../utils/konva/makeInputText';
 
 	let stage: Konva.Stage;
 	let layer: Konva.Layer;
+
+	// 엔티티가 스폰되는 시작 위치
+	let startX = 50;
+	let startY = 50;
+
+	// 엔티티의 첫번째 타이틀 행 정보
+	let titleRowHeight = 30;
+	let titleRowFontSize = 20;
+
+	// 엔티티 나머지 행의 세로 크기
+	let rowHeight = 40;
+
+	// 엔티티의 기본 가로 크기
+	let rowDefaultWidth = 300;
+
+	const entityMap: Map<string, Konva.Rect> = new Map();
 
 	onMount(() => {
 		stage = new Konva.Stage({
@@ -27,17 +44,49 @@
 
 	export async function createEntity() {
 		// create shape
-		var box = new Konva.Rect({
-			x: 50,
-			y: 50,
-			width: 100,
-			height: 50,
-			fill: '#00D2FF',
-			stroke: 'black',
-			strokeWidth: 4,
+
+		const newEntityGroup = new Konva.Group({
+			x: startX,
+			y: startY,
 			draggable: true
 		});
-		layer.add(box);
+
+		const titleRowGroup = new Konva.Group({
+			x: startX,
+			y: startY
+		});
+
+		titleRowGroup.add(
+			new Konva.Rect({
+				width: rowDefaultWidth,
+				height: titleRowHeight,
+				fill: 'black',
+				stroke: 'black',
+				strokeWidth: 4
+			})
+		);
+
+		const entityNameText = makeInputText(
+			stage,
+			new Konva.Text({
+				width: rowDefaultWidth,
+				height: titleRowHeight,
+				text: 'Empty',
+				fill: 'white',
+				fontSize: titleRowFontSize
+			})
+		);
+
+		titleRowGroup.add(entityNameText);
+
+		newEntityGroup.add(titleRowGroup);
+
+		layer.add(newEntityGroup);
+
+		// TODO: 서버 연동시에는 서버에서 생성된 id를 받아와야 함
+		// const entityId = Date.now().toString();
+
+		// entityMap.set(entityId, newEntity);
 	}
 </script>
 
---
+++ src/lib/utils/konva/makeInputText.ts
@@ -0,0 +1,44 @@
+import type Konva from 'konva';
+
+export function makeInputText(stage: Konva.Stage, textNode: Konva.Text): Konva.Text {
+	textNode.on('dblclick', () => {
+		// create textarea over canvas with absolute position
+
+		// first we need to find position for textarea
+		// how to find it?
+
+		// at first lets find position of text node relative to the stage:
+		var textPosition = textNode.getAbsolutePosition();
+
+		// then lets find position of stage container on the page:
+		var stageBox = stage.container().getBoundingClientRect();
+
+		// so position of textarea will be the sum of positions above:
+		var areaPosition = {
+			x: stageBox.left + textPosition.x,
+			y: stageBox.top + textPosition.y
+		};
+
+		// create textarea and style it
+		var textarea = document.createElement('textarea');
+		document.body.appendChild(textarea);
+
+		textarea.value = textNode.text();
+		textarea.style.position = 'absolute';
+		textarea.style.top = areaPosition.y + 'px';
+		textarea.style.left = areaPosition.x + 'px';
+		textarea.style.width = textNode.width().toString();
+
+		textarea.focus();
+
+		textarea.addEventListener('keydown', function (e) {
+			// hide on enter
+			if (e.keyCode === 13) {
+				textNode.text(textarea.value);
+				document.body.removeChild(textarea);
+			}
+		});
+	});
+
+	return textNode;
+}
