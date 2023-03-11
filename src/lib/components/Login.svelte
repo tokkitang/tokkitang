465dc870e6e8f807d3bea859cc8ab107569713c6
myyrakle <sssang97@naver.com>
2023-03-12 02:53:59 +0900

    refac: remove warnings

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { login } from '../api/auth/login';
-	import { setCookie } from '../utils/cookie'
+	import { setCookie } from '../utils/cookie';
 
-	export let email: string;
-	export let password: string;
+	export let email: string | null = null;
+	export let password: string | null = null;
 
 	export async function doLogin() {
 		if (!email) {
@@ -12,24 +12,29 @@
 		if (!password) {
 			alert('비밀번호를 입력해주세요');
 		}
-		const loginResponse = await login(email, password);
-		if (loginResponse.success) {
-			alert('로그인에 성공했습니다.');
-			console.log(loginResponse.access_token);
 
-			setCookie('access_token', loginResponse.access_token);
-		} else {
-			alert('로그인에 실패했습니다.');
+		if (email !== null && password !== null) {
+			const loginResponse = await login(email, password);
+			if (loginResponse.success) {
+				alert('로그인에 성공했습니다.');
+				console.log(loginResponse.access_token);
+
+				setCookie('access_token', loginResponse.access_token);
+			} else {
+				alert('로그인에 실패했습니다.');
+			}
 		}
 	}
 </script>
 
 <main>
-	email: <input id="email" type="email" bind:value={email} /> <br />
-	password: <input id="password" type="password" bind:value={password} /> <br />
+	<form>
+		email: <input id="email" type="email" bind:value={email} /> <br />
+		password: <input id="password" type="password" autoComplete="off" bind:value={password} />
+		<br />
+	</form>
 	<button on:click={doLogin}>Login</button> <br />
 </main>
 
 <style>
-
 </style>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,19 +3,17 @@
 	import Login from '../../lib/components/Login.svelte';
 </script>
 
-<svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
-</svelte:head>
-
 <div class="text-column">
-	<h1>Login Page</h1>
+	<hr />
 
-	<hr>
+	<Login />
 
-	<Login/>
+	<hr />
 
-	<hr>
-
-	<GithubLogin href="https://github.com/login/oauth/authorize?client_id=09c53661153af8c5b13c&redirect_uri=https://ksauqt5f5er2djql3atquzas4e0ofpla.lambda-url.ap-northeast-2.on.aws/redirect/github"/>
+	<GithubLogin
+		href="https://github.com/login/oauth/authorize?client_id=09c53661153af8c5b13c&redirect_uri=https://ksauqt5f5er2djql3atquzas4e0ofpla.lambda-url.ap-northeast-2.on.aws/redirect/github"
+	/>
 </div>
+
+<style>
+</style>
