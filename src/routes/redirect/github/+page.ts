babeb98c3b299b7a6d131c92951ee75e25d161d9
myyrakle <sssang97@naver.com>
2023-03-16 01:31:39 +0900

    add: login redirect

--- src/routes/redirect/github/+page.ts
+++ src/routes/redirect/github/+page.ts
@@ -5,6 +5,7 @@ import { githubLogin } from '../../../lib/api/auth/github-login';
 import { githubSignup, type GithubSignupResponse } from '../../../lib/api/auth/github-signup';
 import { setCookie } from '../../../lib/utils/cookie';
 import { HTTPError } from 'ky';
+import { movePage } from '../../../../../../../../../Users/sssan/Code/Svelte/tokkitang/src/lib/utils/movePage';
 
 export const ssr = false;
 export const csr = true;
@@ -13,74 +14,74 @@ export async function load({ url }: LoadEvent) {
 	const code = url.searchParams?.get?.('code');
 	console.log('code', code);
 
-	if (!code) {
-		throw redirect(302, '/');
-	}
+	if (code === null) {
+		movePage('/');
+	} else {
+		// 1. github code로 access token 획득
+		// 2. access token으로 정보 조회
+		// 3.
+		//    3.1. 정보가 온전한 경우 가입 API 바로 전송
+		//    3.2. 정보가 온전하지 않은 경우 기존의 가입페이지를 띄움
 
-	// 1. github code로 access token 획득
-	// 2. access token으로 정보 조회
-	// 3.
-	//    3.1. 정보가 온전한 경우 가입 API 바로 전송
-	//    3.2. 정보가 온전하지 않은 경우 기존의 가입페이지를 띄움
+		try {
+			const githubTokenResponse = await getGithubAccessToken(code);
+			const githubAccessToken = githubTokenResponse.access_token;
+			const githubLoginResponse = await githubLogin(githubAccessToken);
+			if (githubLoginResponse.success) {
+				setCookie('access_token', githubLoginResponse.access_token);
 
-	try {
-		const githubTokenResponse = await getGithubAccessToken(code);
-		const githubAccessToken = githubTokenResponse.access_token;
-		const githubLoginResponse = await githubLogin(githubAccessToken);
-		if (githubLoginResponse.success) {
-			setCookie('access_token', githubLoginResponse.access_token);
-
-			throw redirect(302, '/');
-		} else {
-			if (githubLoginResponse.need_signup) {
-				const githubUserInfo = await getGithubUser(githubAccessToken);
-				if (
-					githubUserInfo.email != null &&
-					githubUserInfo.name != null &&
-					githubUserInfo.id != null
-				) {
-					try {
-						const githubSignupResponse = await githubSignup(
-							githubUserInfo.name,
-							githubUserInfo.email,
-							githubAccessToken
-						);
-						if (githubSignupResponse.success) {
-							setCookie('access_token', githubSignupResponse.access_token);
-							throw redirect(302, '/');
-						} else {
-							alert('가입 실패');
-							throw redirect(302, '/');
-						}
-					} catch (error: any) {
-						if (error instanceof HTTPError) {
-							const githubSignupFailResponse: GithubSignupResponse = await error.response.json();
-
-							if (githubSignupFailResponse.email_duplicate) {
-								alert('이미 이메일로 가입된 계정입니다.');
-								throw redirect(302, '/');
+				movePage('/mypage');
+			} else {
+				if (githubLoginResponse.need_signup) {
+					const githubUserInfo = await getGithubUser(githubAccessToken);
+					if (
+						githubUserInfo.email != null &&
+						githubUserInfo.name != null &&
+						githubUserInfo.id != null
+					) {
+						try {
+							const githubSignupResponse = await githubSignup(
+								githubUserInfo.name,
+								githubUserInfo.email,
+								githubAccessToken
+							);
+							if (githubSignupResponse.success) {
+								setCookie('access_token', githubSignupResponse.access_token);
+								movePage('/mypage');
+							} else {
+								alert('가입 실패');
+								movePage('/');
 							}
-						}
+						} catch (error: any) {
+							if (error instanceof HTTPError) {
+								const githubSignupFailResponse: GithubSignupResponse = await error.response.json();
 
-						console.error(error);
-						throw redirect(302, '/');
+								if (githubSignupFailResponse.email_duplicate) {
+									alert('이미 이메일로 가입된 계정입니다.');
+									movePage('/');
+								}
+							}
+
+							console.error(error);
+							movePage('/');
+						}
+					} else {
+						let query = `?github=true`;
+						if (githubUserInfo.email != null) {
+							query += `&email=${githubUserInfo.email}`;
+						}
+						if (githubUserInfo.name != null) {
+							query += `&nickname=${githubUserInfo.name}`;
+						}
+						movePage(`/signup${query}`);
 					}
 				} else {
-					let query = `?github=true`;
-					if (githubUserInfo.email != null) {
-						query += `&email=${githubUserInfo.email}`;
-					}
-					if (githubUserInfo.name != null) {
-						query += `&nickname=${githubUserInfo.name}`;
-					}
-					throw redirect(302, `/signup${query}`);
+					alert('로그인 실패. 존재하지 않는 계정');
+					movePage('/');
 				}
-			} else {
-				alert('로그인 실패. 존재하지 않는 계정');
-				throw redirect(302, '/');
 			}
+		} catch (error) {
+			console.error(error);
 		}
-	} catch (error) {
-		console.error(error);
 	}
 }
