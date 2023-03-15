c4fc2d63ce8dd35e1e62a5f4f75852ea14fe8275
myyrakle <sssang97@naver.com>
2023-03-15 22:44:14 +0900

    remove: test codes

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -2,13 +2,6 @@
 	import Signup from '../../lib/components/Signup.svelte';
 </script>
 
-<svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
-</svelte:head>
-
 <div class="text-column">
-	<h1>Signup Page</h1>
-
 	<Signup />
 </div>
