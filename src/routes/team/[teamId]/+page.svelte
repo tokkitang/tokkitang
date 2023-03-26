f85f306345a397291f95f3ca60483a7a5e636478
myyrakle <sssang97@naver.com>
2023-03-27 04:37:39 +0900

    add: team detail page ssr

--- src/routes/team/[teamId]/+page.svelte
+++ src/routes/team/[teamId]/+page.svelte
@@ -1,45 +1,19 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
-	import type { User } from '../../../lib/types/User';
-	import { getCookie } from '../../../lib/utils/cookie';
-	import { getUserInfo } from '../../../lib/api/user/get-user-info';
 	import teamPlaceholder from '$lib/images/team-placeholder.svg';
 	import type { Team } from '../../../lib/types/Team';
-	import { isLoading } from '../../../lib/store';
-	import { getTeamInfo } from '../../../lib/api/team/get-team-info';
 
-	export const teamId = $page.params.teamId;
-	export let team: Team | null = null;
+	export const teamId: string = $page.params.teamId;
+	export const team: Team = $page.data.team;
 
-	$isLoading = true;
-
-	onMount(async () => {
-		const accessToken = getCookie('access_token');
-
-		if (accessToken) {
-			try {
-				const teamInfoResponse = await getTeamInfo(accessToken, teamId);
-				const teamData = teamInfoResponse.data;
-				team = {
-					name: teamData.name,
-					description: teamData.description,
-					thumbnail_url: teamData.thumbnail_url,
-					id: teamData.id
-				};
-			} catch (error) {
-				console.error(error);
-			}
-		}
-
-		$isLoading = false;
-	});
+	onMount(async () => {});
 </script>
 
 <div class="container">
 	<div class="profile">
 		{#if team?.thumbnail_url}
-			<img class="profile-image" src={team?.thumbnail_url} alt="" />
+			<img class="profile-image" src={team.thumbnail_url} alt="" />
 		{:else}
 			<img class="profile-image" src={teamPlaceholder} alt="" />{/if}
 
