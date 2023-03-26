ca48f80bd1df379fb0fbdb9a6364f825aca57aa0
myyrakle <sssang97@naver.com>
2023-03-27 04:30:22 +0900

    add: team/new page ssr

---
+++ src/routes/team/new/+page.server.ts
@@ -0,0 +1,27 @@
+import type { RequestEvent } from '@sveltejs/kit';
+import type { User } from '../../../lib/types/User';
+import { getUserInfo } from '../../../lib/api/user/get-user-info';
+import { getMyTeamList } from '../../../lib/api/team/get-my-team-list';
+import type { Team } from '../../../lib/types/Team';
+
+export async function load(page: RequestEvent) {
+	const accessToken = page.cookies.get('access_token');
+	let myUserInfo: User | null = null;
+	let isLogin = false;
+
+	if (accessToken) {
+		try {
+			const userInfoResponse = await getUserInfo(accessToken);
+			myUserInfo = { ...userInfoResponse };
+			isLogin = true;
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
+	return {
+		accessToken,
+		myUserInfo,
+		isLogin
+	};
+}
