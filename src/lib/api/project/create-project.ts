34bada9cd4a4086b57db0f953d36635039650325
myyrakle <sssang97@naver.com>
2023-03-28 01:01:35 +0900

    feat: create project

---
+++ src/lib/api/project/create-project.ts
@@ -0,0 +1,18 @@
+import { mainApi } from '../setting';
+
+export async function createProject(
+	access_token: string,
+	team_id: string,
+	name: string,
+	description: string,
+	thumbnail_url: string | null
+): Promise<any> {
+	const response = await mainApi.post('project', {
+		headers: {
+			Authorization: `Bearer ${access_token}`
+		},
+		json: { team_id, name, description, thumbnail_url }
+	});
+
+	return await response.json();
+}
---
+++ src/routes/project/new/+page.server.ts
@@ -0,0 +1,29 @@
+import type { RequestEvent } from '@sveltejs/kit';
+import type { User } from '../../../lib/types/User';
+import { getUserInfoWithFetch } from '../../../lib/api/user/get-user-info';
+
+export async function load(page: RequestEvent) {
+	const accessToken = page.cookies.get('access_token');
+	let myUserInfo: User | null = null;
+	let isLogin = false;
+	let error: any = null;
+
+	if (accessToken) {
+		try {
+			const userInfoResponse = await getUserInfoWithFetch(page.fetch, accessToken);
+			myUserInfo = { ...userInfoResponse };
+			isLogin = true;
+			error = error;
+		} catch (e) {
+			error = String(e);
+			console.error(e);
+		}
+	}
+
+	return {
+		accessToken,
+		myUserInfo,
+		isLogin,
+		error
+	};
+}
