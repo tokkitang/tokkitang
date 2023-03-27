261296974607215f8e9ceeb43aaea01bf286064f
myyrakle <sssang97@naver.com>
2023-03-28 01:03:45 +0900

    add: project detail template

---
+++ src/routes/project/[projectId]/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { movePage } from '../../../lib/utils/movePage';
+	import { isLoading } from '../../../lib/store';
+
+	export async function create() {
+		try {
+		} catch (error) {
+			console.error(error);
+		}
+	}
+</script>
+
+<main class="container" />
+
+<style>
+</style>
