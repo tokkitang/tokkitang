df9d79bbce7603466492298297e82ec9469ac1b4
myyrakle <sssang97@naver.com>
2023-03-28 00:38:46 +0900

    add: CreateProjectButton component

---
+++ src/lib/components/CreateProjectButton.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { movePage } from '../utils/movePage';
+
+	export let teamId: string;
+
+	export async function onClicked() {
+		try {
+			movePage(`/project/new?team_id=${teamId}`);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+</script>
+
+<div class="teams">
+	<button on:click={onClicked}>new Project</button>
+</div>
+
+<style>
+	button {
+		display: block;
+		width: 100%;
+		padding: 10px;
+		border: none;
+		border-radius: 5px;
+		background-color: #333;
+		color: #fff;
+		font-size: 16px;
+		cursor: pointer;
+		transition: background-color 0.2s ease-in-out;
+	}
+</style>
