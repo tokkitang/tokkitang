c3f1872ea81dc8a4c8041e88b031944340293ddb
myyrakle <sssang97@naver.com>
2023-04-01 22:10:09 +0900

    add: fetch data on load

--- src/routes/project/[projectId]/+page.server.ts
+++ src/routes/project/[projectId]/+page.server.ts
@@ -1,19 +1,26 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import type { User } from '../../../lib/types/User';
 import { getUserInfoWithFetch } from '../../../lib/api/user/get-user-info';
+import type { Entity } from '$lib/types/Entity';
+import { getEntityListByProjectIdWithFetch } from '$lib/api/entity/get-project-list';
 
 export async function load(page: RequestEvent) {
+	const projectId = page.params.projectId;
+
 	const accessToken = page.cookies.get('access_token');
 	let myUserInfo: User | null = null;
 	let isLogin = false;
 	let error: any = null;
+	let entityList: Entity[] = [];
 
-	if (accessToken) {
+	if (accessToken && projectId) {
 		try {
 			const userInfoResponse = await getUserInfoWithFetch(page.fetch, accessToken);
 			myUserInfo = { ...userInfoResponse };
 			isLogin = true;
-			error = error;
+
+			const entityListResponse = await getEntityListByProjectIdWithFetch(page.fetch, accessToken, projectId);
+			entityList = entityListResponse.list;
 		} catch (e) {
 			error = String(e);
 			console.error(e);
@@ -24,6 +31,6 @@ export async function load(page: RequestEvent) {
 		accessToken,
 		myUserInfo,
 		isLogin,
-		error
+		error, entityList
 	};
 }
