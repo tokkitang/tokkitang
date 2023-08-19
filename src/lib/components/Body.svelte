564d719eb7673d33d2017d462e9109e4b3a9f569
gimmingyu <gimmingyu0201@gmail.com>
2023-08-20 02:59:02 +0900

    feat: global로 사용할 Body component 추가

---
+++ src/lib/components/Body.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<body>
+    <slot />
+</body>
+
+<style>
+
+    body {
+        padding: 8px;
+        background-color: inherit;
+        width: 100%;
+        height: 100%;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,22 @@
 	import '../app.css';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
+	import Body from '$lib/components/Body.svelte';
 </script>
 
 <div class="app">
 	<Header />
-
-	<main>
+	<Body>
 		<slot />
-	</main>
-
+	</Body>
 	<Footer />
 </div>
 
 <style>
 	:global(html) {
 		background-color: #e0eee0;
+		width: 100%;
+		height: 100%;
 	}
+
 </style>
